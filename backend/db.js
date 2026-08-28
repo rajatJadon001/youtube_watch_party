fe6ed94d7c0aa26b549c9bb0e8024205d6995661
Rajat Jadon <jadonrajat35@gmail.com>
2026-08-28 10:59:58 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://postgres:rajput@localhost:5432/watch_party",
+  connectionString: process.env.DATABASE_URL
 });
 
 async function initDb() {
